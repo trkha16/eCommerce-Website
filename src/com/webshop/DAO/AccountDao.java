9dc313f6eca9d5d4e23d64be1717d23372d239ae
Trần Bình Kha <tbkha1609@gmail.com>
2021-12-25 17:15:45 +0700

    Update register

--- src/com/webshop/DAO/AccountDao.java
+++ src/com/webshop/DAO/AccountDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.servlet.RequestDispatcher;
+
 import com.webshop.JDBC.JDBCConnection;
 import com.webshop.model.Account;
 
@@ -14,6 +16,7 @@ public class AccountDao {
 
 	private static final String CHECK_LOGIN = "select * from product.account\r\n"
 			+ "where username = ? and password = ?;";
+	private static final String INSERT_USERS_SQL = "INSERT INTO `product`.`account` (`username`, `password`, `name`) VALUES (?,?,?);";
 
 	public boolean checkLogin(Account account) {
 		boolean status = false;
@@ -30,5 +33,21 @@ public class AccountDao {
 		}
 
 		return status;
+	}
+
+	public int register(Account account) {
+		int result = 0;
+		try {
+			preparedStatement = connection.prepareStatement(INSERT_USERS_SQL);
+			preparedStatement.setString(1, account.getUsername());
+			preparedStatement.setString(2, account.getPassword());
+			preparedStatement.setString(3, account.getName());
+
+			result = preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+		}
+
+		return result;
 	}
 }
---
+++ src/com/webshop/web/RegisterControl.java
@@ -0,0 +1,71 @@
+package com.webshop.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webshop.DAO.AccountDao;
+import com.webshop.model.Account;
+
+/**
+ * Servlet implementation class RegisterControl
+ */
+@WebServlet("/register")
+public class RegisterControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegisterControl() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		AccountDao accountDao = new AccountDao();
+
+		String username = request.getParameter("user");
+		String password = request.getParameter("pass");
+		String rePass = request.getParameter("repass");
+		String name = request.getParameter("name");
+
+		Account account = new Account();
+		account.setUsername(username);
+		account.setPassword(password);
+		account.setName(name);
+
+		try {
+			if (password.equals(rePass)) {
+				accountDao.register(account);
+				response.sendRedirect("Login.jsp");
+			} else {
+				response.sendRedirect("Login.jsp");
+			}
+
+		} catch (Exception e) {
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
